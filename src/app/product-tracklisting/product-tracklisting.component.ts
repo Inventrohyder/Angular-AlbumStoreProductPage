6caaeba509aaba42e78d9f3609da80c105a2c664
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2019-11-11 20:08:30 +0900

    Inject the product service

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -12,7 +12,7 @@ export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
 
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
