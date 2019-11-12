0eddba56d89169b97a7e501dd4357154a3ca0ca4
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2019-11-12 13:00:04 +0900

    Import product into the product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
